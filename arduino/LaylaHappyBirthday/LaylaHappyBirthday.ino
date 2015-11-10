cb604e6601d55bd4de3be3718873bb0d37bfad96
Scotty Allen <scotty@scottyallen.com>
2015-11-10 14:00:10 +0800

    Working version of happy birthday scroller/rainbow

---
+++ arduino/LaylaHappyBirthday/LaylaHappyBirthday.ino
@@ -0,0 +1,335 @@
+#include <FastLED.h>
+
+#define LED_PIN  6
+
+#define COLOR_ORDER RGB
+#define CHIPSET     WS2811
+
+#define BRIGHTNESS 32
+
+#define  MESSAGE "Happy Birthday Layla! From China with love"
+
+// Helper functions for an two-dimensional XY matrix of pixels.
+// Simple 2-D demo code is included as well.
+//
+//     XY(x,y) takes x and y coordinates and returns an LED index number,
+//             for use like this:  leds[ XY(x,y) ] == CRGB::Red;
+//             No error checking is performed on the ranges of x and y.
+//
+//     XYsafe(x,y) takes x and y coordinates and returns an LED index number,
+//             for use like this:  leds[ XY(x,y) ] == CRGB::Red;
+//             Error checking IS performed on the ranges of x and y, and an
+//             index of "-1" is returned.  Special instructions below
+//             explain how to use this without having to do your own error
+//             checking every time you use this function.
+//             This is a slightly more advanced technique, and
+//             it REQUIRES SPECIAL ADDITIONAL setup, described below.
+
+
+// Params for width and height
+const uint8_t kMatrixWidth = 10;
+const uint8_t kMatrixHeight = 7;
+
+// Param for different pixel layouts
+const bool    kMatrixSerpentineLayout = false;
+// Set 'kMatrixSerpentineLayout' to false if your pixels are
+// laid out all running the same way, like this:
+//
+//     0 >  1 >  2 >  3 >  4
+//                         |
+//     .----<----<----<----'
+//     |
+//     5 >  6 >  7 >  8 >  9
+//                         |
+//     .----<----<----<----'
+//     |
+//    10 > 11 > 12 > 13 > 14
+//                         |
+//     .----<----<----<----'
+//     |
+//    15 > 16 > 17 > 18 > 19
+//
+// Set 'kMatrixSerpentineLayout' to true if your pixels are
+// laid out back-and-forth, like this:
+//
+//     0 >  1 >  2 >  3 >  4
+//                         |
+//                         |
+//     9 <  8 <  7 <  6 <  5
+//     |
+//     |
+//    10 > 11 > 12 > 13 > 14
+//                        |
+//                        |
+//    19 < 18 < 17 < 16 < 15
+//
+// Bonus vocabulary word: anything that goes one way
+// in one row, and then backwards in the next row, and so on
+// is call "boustrophedon", meaning "as the ox plows."
+
+
+// This function will return the right 'led index number' for
+// a given set of X and Y coordinates on your matrix.
+// IT DOES NOT CHECK THE COORDINATE BOUNDARIES.
+// That's up to you.  Don't pass it bogus values.
+//
+// Use the "XY" function like this:
+//
+//    for( uint8_t x = 0; x < kMatrixWidth; x++) {
+//      for( uint8_t y = 0; y < kMatrixHeight; y++) {
+//
+//        // Here's the x, y to 'led index' in action:
+//        leds[ XY( x, y) ] = CHSV( random8(), 255, 255);
+//
+//      }
+//    }
+//
+//
+uint16_t XY( uint8_t x, uint8_t y)
+{
+  uint16_t i;
+
+  if ( kMatrixSerpentineLayout == false) {
+    i = (y * kMatrixWidth) + x;
+  }
+
+  if ( kMatrixSerpentineLayout == true) {
+    if ( y & 0x01) {
+      // Odd rows run backwards
+      uint8_t reverseX = (kMatrixWidth - 1) - x;
+      i = (y * kMatrixWidth) + reverseX;
+    } else {
+      // Even rows run forwards
+      i = (y * kMatrixWidth) + x;
+    }
+  }
+
+  return i;
+}
+
+
+// Once you've gotten the basics working (AND NOT UNTIL THEN!)
+// here's a helpful technique that can be tricky to set up, but
+// then helps you avoid the needs for sprinkling array-bound-checking
+// throughout your code.
+//
+// It requires a careful attention to get it set up correctly, but
+// can potentially make your code smaller and faster.
+//
+// Suppose you have an 8 x 5 matrix of 40 LEDs.  Normally, you'd
+// delcare your leds array like this:
+//    CRGB leds[40];
+// But instead of that, declare an LED buffer with one extra pixel in
+// it, "leds_plus_safety_pixel".  Then declare "leds" as a pointer to
+// that array, but starting with the 2nd element (id=1) of that array:
+//    CRGB leds_with_safety_pixel[41];
+//    const CRGB* leds( leds_plus_safety_pixel + 1);
+// Then you use the "leds" array as you normally would.
+// Now "leds[0..N]" are aliases for "leds_plus_safety_pixel[1..(N+1)]",
+// AND leds[-1] is now a legitimate and safe alias for leds_plus_safety_pixel[0].
+// leds_plus_safety_pixel[0] aka leds[-1] is now your "safety pixel".
+//
+// Now instead of using the XY function above, use the one below, "XYsafe".
+//
+// If the X and Y values are 'in bounds', this function will return an index
+// into the visible led array, same as "XY" does.
+// HOWEVER -- and this is the trick -- if the X or Y values
+// are out of bounds, this function will return an index of -1.
+// And since leds[-1] is actually just an alias for leds_plus_safety_pixel[0],
+// it's a totally safe and legal place to access.  And since the 'safety pixel'
+// falls 'outside' the visible part of the LED array, anything you write
+// there is hidden from view automatically.
+// Thus, this line of code is totally safe, regardless of the actual size of
+// your matrix:
+//    leds[ XYsafe( random8(), random8() ) ] = CHSV( random8(), 255, 255);
+//
+// The only catch here is that while this makes it safe to read from and
+// write to 'any pixel', there's really only ONE 'safety pixel'.  No matter
+// what out-of-bounds coordinates you write to, you'll really be writing to
+// that one safety pixel.  And if you try to READ from the safety pixel,
+// you'll read whatever was written there last, reglardless of what coordinates
+// were supplied.
+
+#define NUM_LEDS (kMatrixWidth * kMatrixHeight)
+CRGB leds_plus_safety_pixel[ NUM_LEDS + 1];
+CRGB* leds( leds_plus_safety_pixel + 1);
+
+uint16_t XYsafe( int x, int y)
+{
+  if ( x >= kMatrixWidth) return -1;
+  if ( y >= kMatrixHeight) return -1;
+  if ( x < 0) return -1;
+  if ( y < 0) return -1;
+  return XY(x, y);
+}
+
+
+static unsigned char Font5x7[] = {
+  0x00, 0x00, 0x00, 0x00, 0x00,// (space)
+  0x00, 0x00, 0x5F, 0x00, 0x00,// !
+  0x00, 0x07, 0x00, 0x07, 0x00,// "
+  0x14, 0x7F, 0x14, 0x7F, 0x14,// #
+  0x24, 0x2A, 0x7F, 0x2A, 0x12,// $
+  0x23, 0x13, 0x08, 0x64, 0x62,// %
+  0x36, 0x49, 0x55, 0x22, 0x50,// &
+  0x00, 0x05, 0x03, 0x00, 0x00,// '
+  0x00, 0x1C, 0x22, 0x41, 0x00,// (
+  0x00, 0x41, 0x22, 0x1C, 0x00,// )
+  0x08, 0x2A, 0x1C, 0x2A, 0x08,// *
+  0x08, 0x08, 0x3E, 0x08, 0x08,// +
+  0x00, 0x50, 0x30, 0x00, 0x00,// ,
+  0x08, 0x08, 0x08, 0x08, 0x08,// -
+  0x00, 0x60, 0x60, 0x00, 0x00,// .
+  0x20, 0x10, 0x08, 0x04, 0x02,// /
+  0x3E, 0x51, 0x49, 0x45, 0x3E,// 0
+  0x00, 0x42, 0x7F, 0x40, 0x00,// 1
+  0x42, 0x61, 0x51, 0x49, 0x46,// 2
+  0x21, 0x41, 0x45, 0x4B, 0x31,// 3
+  0x18, 0x14, 0x12, 0x7F, 0x10,// 4
+  0x27, 0x45, 0x45, 0x45, 0x39,// 5
+  0x3C, 0x4A, 0x49, 0x49, 0x30,// 6
+  0x01, 0x71, 0x09, 0x05, 0x03,// 7
+  0x36, 0x49, 0x49, 0x49, 0x36,// 8
+  0x06, 0x49, 0x49, 0x29, 0x1E,// 9
+  0x00, 0x36, 0x36, 0x00, 0x00,// :
+  0x00, 0x56, 0x36, 0x00, 0x00,// ;
+  0x00, 0x08, 0x14, 0x22, 0x41,// <
+  0x14, 0x14, 0x14, 0x14, 0x14,// =
+  0x41, 0x22, 0x14, 0x08, 0x00,// >
+  0x02, 0x01, 0x51, 0x09, 0x06,// ?
+  0x32, 0x49, 0x79, 0x41, 0x3E,// @
+  0x7E, 0x11, 0x11, 0x11, 0x7E,// A
+  0x7F, 0x49, 0x49, 0x49, 0x36,// B
+  0x3E, 0x41, 0x41, 0x41, 0x22,// C
+  0x7F, 0x41, 0x41, 0x22, 0x1C,// D
+  0x7F, 0x49, 0x49, 0x49, 0x41,// E
+  0x7F, 0x09, 0x09, 0x01, 0x01,// F
+  0x3E, 0x41, 0x41, 0x51, 0x32,// G
+  0x7F, 0x08, 0x08, 0x08, 0x7F,// H
+  0x00, 0x41, 0x7F, 0x41, 0x00,// I
+  0x20, 0x40, 0x41, 0x3F, 0x01,// J
+  0x7F, 0x08, 0x14, 0x22, 0x41,// K
+  0x7F, 0x40, 0x40, 0x40, 0x40,// L
+  0x7F, 0x02, 0x04, 0x02, 0x7F,// M
+  0x7F, 0x04, 0x08, 0x10, 0x7F,// N
+  0x3E, 0x41, 0x41, 0x41, 0x3E,// O
+  0x7F, 0x09, 0x09, 0x09, 0x06,// P
+  0x3E, 0x41, 0x51, 0x21, 0x5E,// Q
+  0x7F, 0x09, 0x19, 0x29, 0x46,// R
+  0x46, 0x49, 0x49, 0x49, 0x31,// S
+  0x01, 0x01, 0x7F, 0x01, 0x01,// T
+  0x3F, 0x40, 0x40, 0x40, 0x3F,// U
+  0x1F, 0x20, 0x40, 0x20, 0x1F,// V
+  0x7F, 0x20, 0x18, 0x20, 0x7F,// W
+  0x63, 0x14, 0x08, 0x14, 0x63,// X
+  0x03, 0x04, 0x78, 0x04, 0x03,// Y
+  0x61, 0x51, 0x49, 0x45, 0x43,// Z
+  0x00, 0x00, 0x7F, 0x41, 0x41,// [
+  0x02, 0x04, 0x08, 0x10, 0x20,// "\"
+  0x41, 0x41, 0x7F, 0x00, 0x00,// ]
+  0x04, 0x02, 0x01, 0x02, 0x04,// ^
+  0x40, 0x40, 0x40, 0x40, 0x40,// _
+  0x00, 0x01, 0x02, 0x04, 0x00,// `
+  0x20, 0x54, 0x54, 0x54, 0x78,// a
+  0x7F, 0x48, 0x44, 0x44, 0x38,// b
+  0x38, 0x44, 0x44, 0x44, 0x20,// c
+  0x38, 0x44, 0x44, 0x48, 0x7F,// d
+  0x38, 0x54, 0x54, 0x54, 0x18,// e
+  0x08, 0x7E, 0x09, 0x01, 0x02,// f
+  0x08, 0x14, 0x54, 0x54, 0x3C,// g
+  0x7F, 0x08, 0x04, 0x04, 0x78,// h
+  0x00, 0x44, 0x7D, 0x40, 0x00,// i
+  0x20, 0x40, 0x44, 0x3D, 0x00,// j
+  0x00, 0x7F, 0x10, 0x28, 0x44,// k
+  0x00, 0x41, 0x7F, 0x40, 0x00,// l
+  0x7C, 0x04, 0x18, 0x04, 0x78,// m
+  0x7C, 0x08, 0x04, 0x04, 0x78,// n
+  0x38, 0x44, 0x44, 0x44, 0x38,// o
+  0x7C, 0x14, 0x14, 0x14, 0x08,// p
+  0x08, 0x14, 0x14, 0x18, 0x7C,// q
+  0x7C, 0x08, 0x04, 0x04, 0x08,// r
+  0x48, 0x54, 0x54, 0x54, 0x20,// s
+  0x04, 0x3F, 0x44, 0x40, 0x20,// t
+  0x3C, 0x40, 0x40, 0x20, 0x7C,// u
+  0x1C, 0x20, 0x40, 0x20, 0x1C,// v
+  0x3C, 0x40, 0x30, 0x40, 0x3C,// w
+  0x44, 0x28, 0x10, 0x28, 0x44,// x
+  0x0C, 0x50, 0x50, 0x50, 0x3C,// y
+  0x44, 0x64, 0x54, 0x4C, 0x44,// z
+  0x00, 0x08, 0x36, 0x41, 0x00,// {
+  0x00, 0x00, 0x7F, 0x00, 0x00,// |
+  0x00, 0x41, 0x36, 0x08, 0x00,// }
+  0x08, 0x08, 0x2A, 0x1C, 0x08,// ->
+  0x08, 0x1C, 0x2A, 0x08, 0x08 // <-
+};
+
+
+// Demo that USES "XY" follows code below
+
+void loop()
+{
+  uint32_t ms = millis();
+  int length = strlen(MESSAGE) * 6 + 20;
+  if (ms / 100 <= length) {
+    DrawTextOneFrame(ms / 100 % length);
+  } else {
+    int32_t yHueDelta32 = ((int32_t)cos16( ms * (27 / 1) ) * (350 / kMatrixWidth));
+    int32_t xHueDelta32 = ((int32_t)cos16( ms * (39 / 1) ) * (310 / kMatrixHeight));
+    DrawRainbowOneFrame( ms / 65536, yHueDelta32 / 32768, xHueDelta32 / 32768);
+  }
+  FastLED.show();
+}
+
+void Clear() {
+  for ( byte y = 0; y < kMatrixHeight; y++) {
+    for ( byte x = 0; x < kMatrixWidth; x++) {
+      leds[ XYsafe(x, y)]  = CHSV(0, 255, 128);
+    }
+  }
+}
+
+unsigned char* Char(unsigned char* font, char c) {
+  return &font[(c - 32) * 5];
+}
+
+void DrawText(char *text, int x, int y) {
+  for (int i = 0; i < strlen(text); i++) {
+    DrawSprite(Char(Font5x7, text[i]), 5, x + i * 6, y);
+  }
+}
+
+void DrawTextOneFrame(int xOffset) {
+  Clear();
+  DrawText(MESSAGE, 10 - xOffset, 0);
+}
+
+void DrawSprite(unsigned char* sprite, int length, int xOffset, int yOffset) {
+  //  leds[ XYsafe(x, y) ] = CHSV(0, 0, 255);
+  for ( byte y = 0; y < 7; y++) {
+    for ( byte x = 0; x < 5; x++) {
+      bool on = (sprite[x] >> (6 - y) & 1) * 255;
+      if (on) {
+        leds[ XYsafe(x + xOffset, y + yOffset)]  = CHSV(44, 255, 255);
+      }
+    }
+  }
+}
+
+
+void DrawRainbowOneFrame( byte startHue8, int8_t yHueDelta8, int8_t xHueDelta8) {
+  byte lineStartHue = startHue8;
+  for ( byte y = 0; y < kMatrixHeight; y++) {
+    lineStartHue += yHueDelta8;
+    byte pixelHue = lineStartHue;
+    for ( byte x = 0; x < kMatrixWidth; x++) {
+      pixelHue += xHueDelta8;
+      leds[ XY(x, y)]  = CHSV( pixelHue, 255, 255);
+    }
+  }
+}
+
+void setup() {
+  FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+  FastLED.setBrightness( BRIGHTNESS );
+}
+
